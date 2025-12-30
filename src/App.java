f686d6bf8cb5b759d942eb19bbfdd18e0ee48497
joyce de souza cunha <desouzacunhajoyce@gmail.com>
2025-12-30 19:08:45 -0300

    Implementa sorteio booleano

--- src/App.java
+++ src/App.java
@@ -440,6 +440,9 @@ public class App {
      * Nível de complexidade: 3 de 10
      */
     static boolean sortearValorBooleano() {
+        Random random = new Random();
+
+        return random.nextBoolean();
         //TODO 32: Implementar método conforme explicação
     }
 
