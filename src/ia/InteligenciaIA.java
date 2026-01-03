d95b665ccd37a2dc6ad44761d68f4eca882088ff
LockyTV-17 <luan.lobo.17@gmail.com>
2026-01-03 18:07:27 -0300

    implementação do metodo 15

--- src/ia/InteligenciaIA.java
+++ src/ia/InteligenciaIA.java
@@ -6,7 +6,7 @@ public class InteligenciaIA {
         StringBuilder posicoes = new StringBuilder();
         int tamanho = tabuleiro.length;
         for (int i = 0; i < tamanho; i++) {
-            for (int j = 0; j < tamanho; i++) {
+            for (int j = 0; j < tamanho; j++) {
                 if (tabuleiro [i][j] ==' ') {
                     posicoes.append(i).append(j).append(";");
                 }
@@ -14,4 +14,21 @@ public class InteligenciaIA {
         }
         return posicoes.toString();
     }
+
+    public static int[] obterJogadaComputador(String posicoesLivres) {
+        String[] vetorPosicoes = posicoesLivres.split(";");
+
+
+        java.util.Random random = new java.util.Random();
+        int indiceSorteado = random.nextInt(vetorPosicoes.length);
+
+
+        String jogadaSorteada = vetorPosicoes[indiceSorteado];
+
+        int linha = jogadaSorteada.charAt(0) - '0';
+        int coluna = jogadaSorteada.charAt(1) - '0';
+
+        return new int[] { linha, coluna };
+    }
+
 }
